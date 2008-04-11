d74d0659f8b3fb410667b5774db684bb106bc52e
Jure Kodžoman <jure@tenalt.com>
2008-04-11 14:35:27 +0000

    * Optimized sku length * Renamed title to more meaningful "name"

--- database/pgsql/products.sql
+++ database/pgsql/products.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE products
 (
-  "sku" varchar(255) NOT NULL,
-  "title" varchar(255) NOT NULL,
+  "sku" varchar(32) NOT NULL,
+  "name" varchar(255) NOT NULL,
   "manufacturer" varchar(255),
   "short_description" text,
   "long_description" text,
