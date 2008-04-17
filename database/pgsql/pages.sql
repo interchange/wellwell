b79bb1df4b76f4ee4d57df44bb832333faa2de4f
Jure Kodžoman <jure@tenalt.com>
2008-04-17 00:55:57 +0000

    * Changed sql's to fit new scheme of installation

--- database/pgsql/pages.sql
+++ database/pgsql/pages.sql
@@ -1,10 +1,3 @@
--- DROP TABLE pages;
-
-CREATE TABLE pages
-(
-  name varchar(255) NOT NULL,
-  "template" varchar(255) NOT NULL,
-  CONSTRAINT name PRIMARY KEY (name)
-) 
-WITHOUT OIDS;
-COMMENT ON TABLE pages IS 'Table stores page names and corresponding templates that are applied to that page.';
+name varchar(255) NOT NULL,
+"template" varchar(255) NOT NULL,
+CONSTRAINT name PRIMARY KEY (name)
--- database/pgsql/products.sql
+++ database/pgsql/products.sql
@@ -1,14 +1,7 @@
--- DROP TABLE products;
-
-CREATE TABLE products
-(
-  "sku" varchar(32) NOT NULL,
-  "name" varchar(255) NOT NULL,
-  "manufacturer" varchar(255),
-  "short_description" text,
-  "long_description" text,
-  "price" numeric(11,2) NOT NULL DEFAULT 0,
-  CONSTRAINT products_pkey PRIMARY KEY (sku)
- ) 
-WITHOUT OIDS;
-COMMENT ON TABLE products IS 'Table containing products definitions';
+"sku" varchar(32) NOT NULL,
+"name" varchar(255) NOT NULL,
+"manufacturer" varchar(255),
+"short_description" text,
+"long_description" text,
+"price" numeric(11,2) NOT NULL DEFAULT 0,
+CONSTRAINT products_pkey PRIMARY KEY (sku)
