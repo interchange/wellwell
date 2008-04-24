cd316c5f5de552c80f8ce2c3df2058517b4129b3
Stefan Hornburg <racke@linuxia.de>
2008-04-24 11:09:30 +0000

    definition of users table adjust UserDB settings accordingly

---
+++ database/mysql/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `uid` int unsigned auto_increment NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`)
+);
