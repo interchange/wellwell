25d725fa65e621c7e05d601ec5d1b0c7e4774703
Stefan Hornburg <racke@linuxia.de>
2008-05-20 09:40:27 +0000

    priority field added, use correct key for MySQL

--- database/mysql/categories.sql
+++ database/mysql/categories.sql
@@ -2,5 +2,6 @@ CREATE TABLE `categories` (
   `code` int unsigned NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `parent` int unsigned NOT NULL,
-  KEY (`code`)
+  `priority` int NOT NULL,
+  PRIMARY KEY (`code`)
 ) CHARACTER SET utf8;
--- database/pgsql/categories.sql
+++ database/pgsql/categories.sql
@@ -3,5 +3,6 @@ CREATE TABLE categories
   "code" serial NOT NULL,
   "name" varchar(255) NOT NULL,
   "parent" int4,
+  "priority" int4,
   CONSTRAINT categories_pkey PRIMARY KEY ("code")
 );
