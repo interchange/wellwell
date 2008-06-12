5417737b3a3f77f595a1847c62a514e25df1831d
Davor Ocelic <docelic@spinlocksolutions.com>
2008-06-12 10:24:01 +0000

    * fix typo

--- database/pgsql/product_images.sql
+++ database/pgsql/product_images.sql
@@ -2,6 +2,6 @@ CREATE TABLE "product_images"
 (
   "sku" varchar(64) NOT NULL,
   "image" int4 NOT NULL,
-  "main" bool DEFAULT FALSE;
+  "main" bool DEFAULT FALSE,
   CONSTRAINT product_images_pkey PRIMARY KEY (sku, image)
 );
