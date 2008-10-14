d71da958aa1f2ea5ebedc64b225549729763f30f
Stefan Hornburg <racke@linuxia.de>
2008-10-14 09:01:40 +0000

    type field added for multiple taxonomies

--- database/mysql/product_categories.sql
+++ database/mysql/product_categories.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `product_categories` (
   `sku` varchar(32) NOT NULL,
   `category` int unsigned NOT NULL,
+  `type` varchar(16) NOT NULL,
   KEY (`sku`),
   KEY (`category`)
 ) CHARACTER SET utf8;
