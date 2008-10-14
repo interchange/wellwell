f99fbcd213ebb3c6f8d018b8ba792074587df029
Stefan Hornburg <racke@linuxia.de>
2008-10-14 09:34:36 +0000

    type field added to multiple taxonomies additional indexes for faster lookups

--- database/pgsql/categories.sql
+++ database/pgsql/categories.sql
@@ -1,10 +1,14 @@
 CREATE TABLE categories
 (
   "code" serial NOT NULL,
-  "name" varchar(255) NOT NULL,
-  "parent" int4,
-  "priority" int4,
-  "uri" varchar(255),
+  "name" varchar(255) NOT NULL DEFAULT '',
+  "description" text NOT NULL DEFAULT '',
+  "type" varchar(16) NOT NULL DEFAULT '',
+  "parent" int4 NOT NULL DEFAULT 0,
+  "priority" int4 NOT NULL DEFAULT 0,
+  "uri" varchar(255) NOT NULL DEFAULT '',
   "inactive" bool DEFAULT false,
   CONSTRAINT categories_pkey PRIMARY KEY ("code")
 );
+CREATE INDEX idx_categories_parent ON categories (parent,type);
+CREATE INDEX idx_categories_uri ON categories (uri);
