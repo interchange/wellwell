833840c49031743b0eb07d4b29266b444af0a606
Stefan Hornburg <racke@linuxia.de>
2008-10-14 08:56:44 +0000

    type field added to multiple taxonomies indexes on parent and uri for faster lookups

--- database/mysql/categories.sql
+++ database/mysql/categories.sql
@@ -1,9 +1,13 @@
 CREATE TABLE `categories` (
   `code` int unsigned NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `type` varchar(16) NOT NULL,
   `parent` int unsigned NOT NULL,
   `priority` int NOT NULL,
   `uri` varchar(255) NOT NULL,
   `inactive` boolean NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (`code`)
+  PRIMARY KEY (`code`),
+  KEY `parent` (`parent`, `type`),
+  KEY `uri` (`uri`)
 ) CHARACTER SET utf8;
