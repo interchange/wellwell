62776d3a180ba7eb36531eb39e8af75bb55a5194
Stefan Hornburg <racke@linuxia.de>
2008-11-17 09:42:35 +0000

    add permission field use NOT NULL in abundance

--- database/mysql/menus.sql
+++ database/mysql/menus.sql
@@ -1,10 +1,11 @@
 CREATE TABLE `menus`
 (
   `code` int unsigned NOT NULL auto_increment,
-  `name` varchar(255),
+  `name` varchar(255) NOT NULL DEFAULT '',
   `parent` int unsigned NOT NULL DEFAULT 0,
-  `url` varchar(255),
-  `menu_name` varchar(64),
+  `url` varchar(255) NOT NULL DEFAULT '',
+  `permission` varchar(64) NOT NULL DEFAULT '',
+  `menu_name` varchar(64) NOT NULL DEFAULT '',
   `weight` int unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`code`),
   KEY (`menu_name`)
--- database/pgsql/menus.sql
+++ database/pgsql/menus.sql
@@ -1,10 +1,11 @@
 CREATE TABLE menus
 (
   "code" serial NOT NULL,
-  "name" varchar(255),
+  "name" varchar(255) NOT NULL DEFAULT '',
   "parent" int4 NOT NULL DEFAULT 0,
-  "url" varchar(255),
-  "menu_name" varchar(64),
+  "url" varchar(255) NOT NULL DEFAULT '',
+  "permission" varchar(64) NOT NULL DEFAULT '',
+  "menu_name" varchar(64) NOT NULL DEFAULT '',
   "weight" int4 NOT NULL DEFAULT 0,
   CONSTRAINT menu_pkey PRIMARY KEY (code)
 );
