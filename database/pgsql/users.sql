d7a36f3eca9661911f36a086b3e58e04bc719013
Stefan Hornburg <racke@linuxia.de>
2008-11-18 13:11:38 +0000

    not null columns need a default in PostgreSQL

--- database/pgsql/users.sql
+++ database/pgsql/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "users" (
   "uid" serial,
   "username" varchar(32),
-  "email" varchar(255) NOT NULL,
-  "password" varchar(255) NOT NULL,
+  "email" varchar(255) NOT NULL DEFAULT '',
+  "password" varchar(255) NOT NULL DEFAULT '',
   CONSTRAINT users_pkey PRIMARY KEY ("uid")
 );
