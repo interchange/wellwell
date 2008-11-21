ccf981d1a00c239ffda65e3baab29c2d3dfccd4d
Stefan Hornburg <racke@linuxia.de>
2008-11-21 12:03:51 +0000

    remove spurious statement

--- database/pgsql/product_categories.sql
+++ database/pgsql/product_categories.sql
@@ -4,8 +4,7 @@ CREATE TABLE "product_categories"
   "category" int4 NOT NULL,
   "type" varchar(16) NOT NULL,
   CONSTRAINT product_categories_pkey PRIMARY KEY ("sku", "category")
-) 
-WITHOUT OIDS;
+); 
 CREATE INDEX idx_product_categories_sku ON product_categories (sku);
 CREATE INDEX idx_product_categories_category ON product_categories (category);
 
