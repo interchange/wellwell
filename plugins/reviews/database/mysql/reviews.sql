f0f2318fc3d3b9b6c927888a50651baa8e48c33a
Stefan Hornburg <racke@linuxia.de>
2008-12-08 14:21:37 +0000

    name and public fields added

--- plugins/reviews/database/mysql/reviews.sql
+++ plugins/reviews/database/mysql/reviews.sql
@@ -2,9 +2,11 @@ CREATE TABLE reviews (
   code integer NOT NULL auto_increment PRIMARY KEY,
   sku varchar(32) NOT NULL,
   uid integer NOT NULL,
+  name varchar(255) NOT NULL DEFAULT '',
   created datetime NOT NULL,
   rating integer NOT NULL DEFAULT 0,
   title varchar(255) NOT NULL DEFAULT '',
+  public boolean NOT NULL DEFAULT FALSE,
   review text NOT NULL DEFAULT '',
   KEY(sku)  
 );
--- plugins/reviews/database/pgsql/reviews.sql
+++ plugins/reviews/database/pgsql/reviews.sql
@@ -2,9 +2,11 @@ CREATE TABLE reviews (
   code serial PRIMARY KEY,
   sku varchar(32) NOT NULL,
   uid integer NOT NULL,
+  name varchar(255) NOT NULL DEFAULT '',
   created timestamp NOT NULL,
   rating integer NOT NULL DEFAULT 0,
   title varchar(255) NOT NULL DEFAULT '',
+  public boolean NOT NULL DEFAULT FALSE,
   review text NOT NULL DEFAULT ''
 );
 CREATE INDEX reviews_sku ON reviews(sku);
