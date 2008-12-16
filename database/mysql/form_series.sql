c8c351908429b2f770aa4e55aee31e13de10f141
Stefan Hornburg <racke@linuxia.de>
2008-12-16 20:44:56 +0000

    bring database structure up-to-date with upcoming [form]/[compose] changes

--- database/mysql/form_series.sql
+++ database/mysql/form_series.sql
@@ -1,10 +1,13 @@
 CREATE TABLE form_series (
   name varchar(255) NOT NULL DEFAULT '',
-  component varchar(255) NOT NULL DEFAULT '',
+  part varchar(255) NOT NULL DEFAULT '',
   label varchar(255) NOT NULL DEFAULT '',
+  progress_label varchar(255) NOT NULL DEFAULT '',
+  template varchar(255) NOT NULL DEFAULT '',
   profile varchar(255) NOT NULL DEFAULT '',
   position int unsigned NOT NULL DEFAULT 0,
   `load` varchar(255) NOT NULL DEFAULT '',
   save varchar(255) NOT NULL DEFAULT '',
+  apply varchar(255) NOT NULL DEFAULT '', 
   key(name)
 );
--- database/pgsql/form_series.sql
+++ database/pgsql/form_series.sql
@@ -1,11 +1,14 @@
 CREATE TABLE "form_series" (
   name varchar(255) NOT NULL DEFAULT '',
-  component varchar(255) NOT NULL DEFAULT '',
+  part varchar(255) NOT NULL DEFAULT '',
   label varchar(255) NOT NULL DEFAULT '',
+  progress_label varchar(255) NOT NULL DEFAULT '',
+  template varchar(255) NOT NULL DEFAULT '',
   profile varchar(255) NOT NULL DEFAULT '',
   position integer NOT NULL DEFAULT 0,
   load varchar(255) NOT NULL DEFAULT '',
-  save varchar(255) NOT NULL DEFAULT ''
+  save varchar(255) NOT NULL DEFAULT '',
+  apply varchar(255) NOT NULL DEFAULT '',
 );
 CREATE INDEX form_series_name ON form_series(name);
 
