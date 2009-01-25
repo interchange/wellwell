82906a83425b6edfbfd5a15e82fc664d34aca79d
Stefan Hornburg <racke@linuxia.de>
2009-01-25 08:58:29 +0000

    table structures for permissions

---
+++ database/mysql/permissions.sql
@@ -0,0 +1,7 @@
+CREATE TABLE permissions (
+  rid int not null default 0,
+  uid int not null default 0,
+  perm varchar(255) not null default ''
+);
+
+
---
+++ database/pgsql/permissions.sql
@@ -0,0 +1,6 @@
+CREATE TABLE permissions (
+  rid integer not null default 0,
+  uid integer not null default 0,
+  perm varchar(255) not null default ''
+);
+
