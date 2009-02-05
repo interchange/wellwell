4ee68f65ac4e251b351376170a70c7614994de9b
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-02-05 23:59:29 +0100

    clean up, add uri column

--- plugins/content/database/mysql/content.sql
+++ plugins/content/database/mysql/content.sql
@@ -1,11 +1,12 @@
-CREATE TABLE `content` (
-  `code` int unsigned NOT NULL auto_increment,
-  `type` varchar(32) NOT NULL,
-  `uid` int unsigned NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` longtext NOT NULL,
-  `locale` varchar(255) NOT NULL default 'en_US',
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`code`),
-  KEY (`type`)
-) CHARACTER SET utf8;
+CREATE TABLE content (
+  code int unsigned NOT NULL auto_increment,
+  type varchar(32) NOT NULL,
+  uid int unsigned NOT NULL,
+  title varchar(255) NOT NULL,
+  body longtext NOT NULL,
+  uri varchar(255) NOT NULL,
+  locale varchar(255) NOT NULL default 'en_US',
+  created datetime NOT NULL,
+  PRIMARY KEY (code),
+  KEY (type)
+);
--- plugins/content/database/pgsql/content.sql
+++ plugins/content/database/pgsql/content.sql
@@ -1,10 +1,11 @@
-CREATE TABLE "content" (
-  "code" serial NOT NULL,
-  "type" varchar(32) NOT NULL,
-  "uid" int8 NOT NULL,
-  "title" varchar(255) NOT NULL,
-  "body" text NOT NULL,
-  "locale" varchar(255) NOT NULL DEFAULT 'en_US'::character varying,
-  "created" timestamp NOT NULL,
-  PRIMARY KEY ("code")
+CREATE TABLE content (
+  code serial NOT NULL,
+  type varchar(32) NOT NULL,
+  uid int8 NOT NULL,
+  title varchar(255) NOT NULL,
+  body text NOT NULL,
+  uri varchar(255) NOT NULL,
+  locale varchar(255) NOT NULL DEFAULT 'en_US',
+  created timestamp NOT NULL,
+  PRIMARY KEY (code)
 );
