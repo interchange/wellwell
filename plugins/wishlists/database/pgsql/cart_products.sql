50735e51d7712a3117b316067ff9ffe5e01b29ce
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-04-02 09:35:41 +0200

    primary key added for cart_products table

--- plugins/wishlists/database/pgsql/cart_products.sql
+++ plugins/wishlists/database/pgsql/cart_products.sql
@@ -1,5 +1,6 @@
 CREATE TABLE cart_products (
   cart integer NOT NULL,
   sku varchar(32) NOT NULL,
-  position integer NOT NULL
+  position integer NOT NULL,
+  CONSTRAINT cart_products_pkey PRIMARY KEY (cart, position)
 );
