d68c878e714b223b226f3ffd6bcf971e661f034c
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-05-29 19:46:32 +0200

    added created and last_login fields to users table

--- database/mysql/users.sql
+++ database/mysql/users.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
   username varchar(32) NOT NULL,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
+  last_login integer NOT NULL DEFAULT 0,
+  created integer NOT NULL DEFAULT 0,
   PRIMARY KEY (uid)
 );
-
--- database/pgsql/users.sql
+++ database/pgsql/users.sql
@@ -3,5 +3,7 @@ CREATE TABLE users (
   username varchar(32) NOT NULL,
   email varchar(255) NOT NULL DEFAULT '',
   password varchar(255) NOT NULL DEFAULT '',
+  last_login integer NOT NULL DEFAULT 0,
+  created integer NOT NULL DEFAULT 0,
   CONSTRAINT users_pkey PRIMARY KEY (uid)
 );
