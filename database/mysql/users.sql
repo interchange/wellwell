39a004febd4e801c795fe760de3d083038a11590
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-05-29 19:41:21 +0200

    get rid of unnecessary backticks

--- database/mysql/users.sql
+++ database/mysql/users.sql
@@ -1,7 +1,8 @@
-CREATE TABLE `users` (
-  `uid` int unsigned auto_increment NOT NULL,
-  `username` varchar(32) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`uid`)
+CREATE TABLE users (
+  uid int unsigned auto_increment NOT NULL,
+  username varchar(32) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (uid)
 );
+
