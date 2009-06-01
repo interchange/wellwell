085f7e641109610d26a790a656fa90cabb76b920
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-06-01 13:23:29 +0200

    get rid of backticks, quotes and character set

--- database/mysql/product_images.sql
+++ database/mysql/product_images.sql
@@ -1,7 +1,8 @@
-CREATE TABLE `product_images`
+CREATE TABLE product_images
 (
-  `sku` varchar(64) NOT NULL,
-  `image` int unsigned NOT NULL,
-  `main` boolean DEFAULT false,
-  PRIMARY KEY (`sku`, `image`)
-) CHARACTER SET utf8;
+  sku varchar(64) NOT NULL,
+  image int unsigned NOT NULL,
+  main boolean DEFAULT false,
+  PRIMARY KEY (sku, image)
+);
+
--- database/pgsql/product_images.sql
+++ database/pgsql/product_images.sql
@@ -1,7 +1,7 @@
-CREATE TABLE "product_images"
+CREATE TABLE product_images
 (
-  "sku" varchar(64) NOT NULL,
-  "image" int4 NOT NULL,
-  "main" bool DEFAULT FALSE,
+  sku varchar(64) NOT NULL,
+  image int4 NOT NULL,
+  main bool DEFAULT FALSE,
   CONSTRAINT product_images_pkey PRIMARY KEY (sku, image)
 );
