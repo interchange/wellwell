fc9729f57bd2c11ddeb7c81b9de028c57e0109c5
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-06-01 11:18:29 +0200

    get rid of backticks, quotes and character set

--- database/mysql/images.sql
+++ database/mysql/images.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `images`
+CREATE TABLE images
 (
-  `code` int unsigned NOT NULL auto_increment,
-  `name` varchar(255),
-  `format` char(3),
-  `created` datetime,
-  `author` int unsigned,
-  `inactive` boolean DEFAULT FALSE,
-  PRIMARY KEY (`code`)
-)  CHARACTER SET utf8;
+  code int unsigned NOT NULL auto_increment,
+  name varchar(255),
+  format char(3),
+  created datetime,
+  author int unsigned,
+  inactive boolean DEFAULT FALSE,
+  PRIMARY KEY (code)
+)
--- database/pgsql/images.sql
+++ database/pgsql/images.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "images"
+CREATE TABLE images
 (
-  "code" serial NOT NULL,
-  "name" varchar(255),
-  "format" char(3),
-  "created" timestamp,
-  "author" int4,
-  "inactive" bool DEFAULT false,
-  CONSTRAINT images_pkey PRIMARY KEY ("code")
+  code serial NOT NULL,
+  name varchar(255),
+  format char(3),
+  created timestamp,
+  author int4,
+  inactive bool DEFAULT false,
+  CONSTRAINT images_pkey PRIMARY KEY (code)
 );
