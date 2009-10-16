c2771d106d30694fd6c8c0f70a198b07593db99d
Stefan Hornburg (Racke) <racke@linuxia.de>
2009-10-16 19:20:51 +0200

    put index definition inside of create table statement

--- database/mysql/product_attributes.sql
+++ database/mysql/product_attributes.sql
@@ -1,6 +1,7 @@
 CREATE TABLE product_attributes (
 	sku varchar(32) NOT NULL,
 	name varchar(255) NOT NULL,
-	value text NOT NULL DEFAULT ''
+	value text NOT NULL DEFAULT '',
+	KEY(sku)
 );
-CREATE INDEX product_attributes_sku ON product_attributes (sku);
+
