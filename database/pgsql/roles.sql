4febf49d940832a1224d70c4d67f8af11fdc83ee
Stefan Hornburg (Racke) <racke@linuxia.de>
2010-06-02 12:45:56 +0200

    Create records in roles for default roles anonymous and authenticated.

--- database/pgsql/roles.sql
+++ database/pgsql/roles.sql
@@ -4,3 +4,6 @@ CREATE TABLE "roles" (
   "label" varchar(255) NOT NULL,
   CONSTRAINT roles_pkey PRIMARY KEY ("rid")
 );
+
+INSERT INTO roles (rid,name,label) VALUES (1, 'anonymous', 'Anonymous Users');
+INSERT INTO roles (rid,name,label) VALUES (2, 'authenticated', 'Authenticated Users');
