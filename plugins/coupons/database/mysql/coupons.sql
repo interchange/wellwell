a23fe13407531e7283077ca3ebc84d8fbc1a7fd3
Stefan Hornburg (Racke) <racke@linuxia.de>
2010-07-20 16:36:50 +0200

    Add missing field inactive to database definition for coupons table.

--- plugins/coupons/database/mysql/coupons.sql
+++ plugins/coupons/database/mysql/coupons.sql
@@ -2,5 +2,6 @@ CREATE TABLE coupons (
 	code int NOT NULL auto_increment,
 	coupon_number varchar(32) not null default '',
     amount decimal(11,2) NOT NULL DEFAULT 0,
+	inactive boolean NOT NULL DEFAULT FALSE,
 	PRIMARY KEY(code)
 );
