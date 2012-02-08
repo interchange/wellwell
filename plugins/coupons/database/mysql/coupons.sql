5df024e72b294132c97112133ac41986d01eaa37
Jure Kodžoman <jure.kodzoman@informa.si>
2012-02-08 09:47:00 +0100

    * added count columns

--- plugins/coupons/database/mysql/coupons.sql
+++ plugins/coupons/database/mysql/coupons.sql
@@ -1,7 +1,8 @@
 CREATE TABLE coupons (
 	code int NOT NULL auto_increment,
 	coupon_number varchar(32) not null default '',
-    amount decimal(11,2) NOT NULL DEFAULT 0,
+   amount decimal(11,2) NOT NULL DEFAULT 0,
 	inactive boolean NOT NULL DEFAULT FALSE,
+	coupons int NOT NULL DEFAULT 1000000,
 	PRIMARY KEY(code)
 );
